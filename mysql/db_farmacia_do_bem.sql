c4bd79ccdd132d311ea4fa0a4eb4b0c560ce29b1
eralima <erikalmlima@gmail.com>
2021-05-22 16:14:00 -0300

    Exercícios de INNER JOIN em MySQL

--- mysql/db_farmacia_do_bem.sql
+++ mysql/db_farmacia_do_bem.sql
@@ -7,16 +7,18 @@ use db_farmacia_do_bem;
 create table tb_categoria (
 	id bigint auto_increment,
     descricao varchar(255) not null,
-    ativo boolean not null,
+    oferta_fabricante boolean,
+    oferta_farmacia boolean,
+    delivery boolean not null,
     primary key (id)
 );
 
 -- popule esta tabela categoria com até 5 dados.
-insert into tb_categoria (descricao, ativo) values ("Medicamentos", true);
-insert into tb_categoria (descricao, ativo) values ("Genericos", true);
-insert into tb_categoria (descricao, ativo) values ("Beleza e cosméticos", true);
-insert into tb_categoria (descricao, ativo) values ("Higiene", true);
-insert into tb_categoria (descricao, ativo) values ("Infantil", true);
+insert into tb_categoria (descricao, oferta_fabricante, oferta_farmacia, delivery) values ("Medicamentos", false, true, false);
+insert into tb_categoria (descricao, oferta_fabricante, oferta_farmacia, delivery) values ("Genericos", true, false, false );
+insert into tb_categoria (descricao, oferta_fabricante, oferta_farmacia, delivery) values ("Beleza e cosméticos", false, true, true);
+insert into tb_categoria (descricao, oferta_fabricante, oferta_farmacia, delivery) values ("Higiene", false, true, true);
+insert into tb_categoria (descricao, oferta_fabricante, oferta_farmacia, delivery) values ("Infantil", false, true, true);
 
 -- crie uma tabela de tb_produto com 5 atributos relevantes para se trabalhar com o serviço deste farmacia
 create table tb_produtos(
@@ -30,6 +32,8 @@ create table tb_produtos(
     primary key(id),
     foreign key (categoria_id) references tb_categoria (id)
 );
+
+select * from tb_categoria;
 
 -- popule esta tabela produto com até 8 dados
 insert into tb_produtos (nome, marca, categoria, preco, quantidade_produto,categoria_id) values ("Prednisona 20mg 10 comprimidos", "Medley", "Corticosteróides", 6.80, 200, 2);
@@ -58,5 +62,6 @@ select nome, marca, categoria, preco, tb_categoria.descricao as categoria_geral
 from tb_produtos inner join tb_categoria on  tb_categoria.id = tb_produtos.categoria_id;
 
 -- faça um select onde traga todos os produtos de uma categoria específica (exemplo todos os produtos que são cosméticos).
-select * from tb_produtos inner join tb_categoria on  tb_categoria.id = tb_produtos.categoria_id
+select nome, marca, categoria, preco, tb_categoria.descricao as categoria_geral
+from tb_produtos inner join tb_categoria on  tb_categoria.id = tb_produtos.categoria_id
 where tb_categoria.descricao like "Beleza e cosméticos";
